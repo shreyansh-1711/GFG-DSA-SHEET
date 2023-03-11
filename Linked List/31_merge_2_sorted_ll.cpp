3f64e52ae699030fa07065cbafe5c5efe26683c5
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-12 01:25:36 +0530

    Linked List

--- Linked List/31_merge_2_sorted_ll.cpp
+++ Linked List/31_merge_2_sorted_ll.cpp
@@ -1,67 +1,67 @@
-/************************************************************
-    Following is the linked list node structure.
+// /************************************************************
+//     Following is the linked list node structure.
     
-    template <typename T>
-    class Node {
-        public:
-        T data;
-        Node* next;
-        Node(T data) {
-            next = NULL;
-            this->data = data;
-        }
-        ~Node() {
-            if (next != NULL) {
-                delete next;
-            }
-        }
-    };
-************************************************************/
+//     template <typename T>
+//     class Node {
+//         public:
+//         T data;
+//         Node* next;
+//         Node(T data) {
+//             next = NULL;
+//             this->data = data;
+//         }
+//         ~Node() {
+//             if (next != NULL) {
+//                 delete next;
+//             }
+//         }
+//     };
+// ************************************************************/
 
-void solve(Node<int>* first, Node<int>* second) {
+// void solve(Node<int>* first, Node<int>* second) {
     
     
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
+//     Node* curr1 = first;
+//     Node* next1 = curr1 -> next;
     
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
+//     Node* curr2 = second;
+//     Node* next2 = curr2 -> next;
     
-    while(next1 != NULL && curr2 != NULL) {
+//     while(next1 != NULL && curr2 != NULL) {
         
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
+//         if( (curr2 -> data >= curr1 -> data ) 
+//            && ( curr2 -> data <= next1 -> data)) {
             
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
-            curr1 = curr2;
-            curr2 = next2;
-        }
-        else {
+//             curr1 -> next = curr2;
+//             curr2 -> next = next1;
+//             curr1 = curr2;
+//             curr2 = next2;
+//         }
+//         else {
             
-        }
+//         }
         
         
-    }
+//     }
     
     
-}
+// }
 
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
-{
-    if(first == NULL)
-        return second;
+// Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+// {
+//     if(first == NULL)
+//         return second;
     
-    if(second == NULL)
-        return first;
+//     if(second == NULL)
+//         return first;
     
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
-    }
+//     if(first -> data <= second -> data ){
+//         solve(first, second);
+//     }
+//     else
+//     {
+//         solve(second, first);
+//     }
     
     
-}
+// }
