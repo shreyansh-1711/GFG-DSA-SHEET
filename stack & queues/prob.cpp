229de881b119b986efccadedb4f4db0db4ec449a
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-19 22:11:59 +0530

    queue

--- stack & queues/prob.cpp
+++ stack & queues/prob.cpp
@@ -1,95 +1,54 @@
 
-#include<bits/stdc++.h>
+
+#include<iostream>
+#include<queue>
 using namespace std;
 
-class Solution 
-{
-    private:
-    bool knows(vector<vector<int> >& M, int a, int b, int n) {
-        if(M[a][b] == 1)
-            return true;
-        else
-            return false;
-    }
-    public:
-    //Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M, int n) 
-    {
-        stack<int> s;
-        //step1: push all element in stack
-        for(int i=0; i<n; i++) {
-            s.push(i);
-        }   
-        
-        //step2: get 2 elements and copare them
-        
-        while(s.size() > 1) {
-            
-            int a = s.top();
-            s.pop();
-            
-            int b = s.top();
-            s.pop();
-            
-            if(knows(M,a,b,n)){
-                s.push(b);
-            }
-            else
-            {
-                s.push(a);
-            }
-        }
-        int ans = s.top();
-        //step3: single element in stack is potential celeb
-        //so verify it
-        
-        int zeroCount = 0;
-        
-        for(int i=0; i<n; i++) {
-            if(M[ans][i] == 0)
-                zeroCount++;
-        }
-        
-        //all zeroes
-        if(zeroCount != n)
-            return -1;
-        
-        //column check
-        int oneCount = 0;
-        
-        for(int i=0; i<n; i++) {
-            if(M[i][ans] == 1)
-                oneCount++;
-        }
-        
-        if(oneCount != n-1)
-            return -1;
-        
-        return ans;
-        
-    }
-};
+int main() {
+    cout << (-1)%1<< endl;
+    deque<int> d;
 
-// { Driver Code Starts.
+    d.push_front(12);
+    d.push_back(14);
 
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<vector<int> > M( n , vector<int> (n, 0));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                cin>>M[i][j];
-            }
-        }
-        Solution ob;
-        cout<<ob.celebrity(M,n)<<endl;
+    cout << d.front() << endl;
+    cout << d.back() << endl;
 
+    d.pop_front();
+    
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+    d.pop_back();
+
+    if(d.empty()) {
+        cout << "queue is empty" << endl;
     }
+    else{
+        cout << "queue is not empty " << endl;
+    }
+
+    /*
+    //create a queue
+    queue<int> q;
+    q.push(11);
+    cout << "front of q is: " << q.front() << endl;
+    q.push(15);
+    cout << "front of q is: " << q.front() << endl;
+    q.push(13);
+    cout << "front of q is: " << q.front() << endl;
+    cout << "size of queue is : " << q.size() << endl;
+    q.pop();
+    q.pop();
+    q.pop();
+    cout << "size of queue is : " << q.size() << endl;
+    if(q.empty()) {
+        cout << "Queue is empty " << endl;
+    }
+    else{
+        cout << "queue is not empty " << endl;
+    }
+    */
+
+
+    return 0;
 }
