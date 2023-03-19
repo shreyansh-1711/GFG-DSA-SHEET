2e8f3430cc07841e7494627c657b5c921e97a783
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-19 22:12:52 +0530

    queue

--- stack & queues/prob.cpp
+++ stack & queues/prob.cpp
@@ -1,54 +1,116 @@
 
 
-#include<iostream>
-#include<queue>
-using namespace std;
+// #include<iostream>
+// #include<queue>
+// using namespace std;
 
-int main() {
-    cout << (-1)%1<< endl;
-    deque<int> d;
+// int main() {
+//     cout << (-1)%1<< endl;
+//     deque<int> d;
 
-    d.push_front(12);
-    d.push_back(14);
+//     d.push_front(12);
+//     d.push_back(14);
 
-    cout << d.front() << endl;
-    cout << d.back() << endl;
+//     cout << d.front() << endl;
+//     cout << d.back() << endl;
 
-    d.pop_front();
+//     d.pop_front();
     
-    cout << d.front() << endl;
-    cout << d.back() << endl;
-    d.pop_back();
+//     cout << d.front() << endl;
+//     cout << d.back() << endl;
+//     d.pop_back();
 
-    if(d.empty()) {
-        cout << "queue is empty" << endl;
-    }
-    else{
-        cout << "queue is not empty " << endl;
-    }
+//     if(d.empty()) {
+//         cout << "queue is empty" << endl;
+//     }
+//     else{
+//         cout << "queue is not empty " << endl;
+//     }
 
-    /*
-    //create a queue
-    queue<int> q;
-    q.push(11);
-    cout << "front of q is: " << q.front() << endl;
-    q.push(15);
-    cout << "front of q is: " << q.front() << endl;
-    q.push(13);
-    cout << "front of q is: " << q.front() << endl;
-    cout << "size of queue is : " << q.size() << endl;
-    q.pop();
-    q.pop();
-    q.pop();
-    cout << "size of queue is : " << q.size() << endl;
-    if(q.empty()) {
-        cout << "Queue is empty " << endl;
-    }
-    else{
-        cout << "queue is not empty " << endl;
-    }
-    */
+//     /*
+//     //create a queue
+//     queue<int> q;
+//     q.push(11);
+//     cout << "front of q is: " << q.front() << endl;
+//     q.push(15);
+//     cout << "front of q is: " << q.front() << endl;
+//     q.push(13);
+//     cout << "front of q is: " << q.front() << endl;
+//     cout << "size of queue is : " << q.size() << endl;
+//     q.pop();
+//     q.pop();
+//     q.pop();
+//     cout << "size of queue is : " << q.size() << endl;
+//     if(q.empty()) {
+//         cout << "Queue is empty " << endl;
+//     }
+//     else{
+//         cout << "queue is not empty " << endl;
+//     }
+//     */
 
 
-    return 0;
-}
+//     return 0;
+// }class Queue {
+    
+//     int* arr;
+//     int qfront;
+//     int rear; 
+//     int size;
+    
+// public:
+//     Queue() {
+//         size = 100001;
+//         arr = new int[size];
+//         qfront = 0;
+//         rear = 0;
+//     }
+
+//     /*----------------- Public Functions of Queue -----------------*/
+
+//     bool isEmpty() {
+//         if(qfront == rear) {
+//             return true;
+//         }
+//         else
+//         {
+//             return false;
+//         }
+//     }
+
+//     void enqueue(int data) {
+//         if(rear == size)
+//             cout << "Queue is Full" << endl;
+//         else
+//         {
+//             arr[rear] = data;
+//             rear++;
+//         }
+//     }
+
+//     int dequeue() {
+//         if(qfront == rear) {
+//             return -1;
+//         }
+//         else
+//         {	int ans = arr[qfront];
+//             arr[qfront] = -1;
+//             qfront++;
+//             if(qfront==rear){
+//                 qfront = 0;
+//                 rear = 0;
+//             }
+//          return ans;
+//         }
+//     }
+
+//     int front() {
+//         if(qfront == rear) {
+//             return -1;
+//         }
+//         else
+//         {
+//             return arr[qfront];
+//         }
+//     }
+// };
