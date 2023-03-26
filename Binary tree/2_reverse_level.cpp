f6ec0047b0ec55eba9794261659eb6afd95f6673
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-26 11:35:02 +0530

    leverorder and reverselevel order

---
+++ Binary tree/2_reverse_level.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int data;   
+        node *left;
+        node *right;
+
+
+    node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }    
+
+};
+
+node* buildTree(node* root){
+    cout << "Enter the data " << endl;
+    int data;
+    cin>>data;
+    root = new node(data);
+    if(data == -1){
+        return NULL;
+    }
+
+    cout << "Enter the left child " << data << endl;
+    root->left = buildTree(root->left);
+
+    cout << "Enter the right child " << data << endl;
+    root->right = buildTree(root->right);
+    return root;                
+}
+
+void levelordertraversal(node* root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        
+        
+
+
+    if(temp == NULL){
+        cout << endl;
+        if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+    else{
+
+        cout << temp->data << " ";
+        if(temp->left!= NULL){
+            q.push(temp->left);
+        }
+        if(temp->right!= NULL){
+            q.push(temp->right);
+        }
+    }
+}
+}
+
+vector<int> reverseLevelOrder(node *root)
+{
+    // code here
+     vector<int>ans;
+    stack<int>st;
+    queue<node*>q;
+    q.push(root);
+    
+    if(root==NULL){
+        return ans;
+    }
+    while(!q.empty()){
+        node *s=q.front();
+        q.pop();
+        st.push(s->data);
+        if(s->right) q.push(s->right);
+        if(s->left)  q.push(s->left);
+        
+    }
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
+
+
+int main(){
+
+    node* root = NULL;
+    root = buildTree(root);
+
+    // cout << "Level Order traversing : " ;
+    // levelordertraversal(root);
+
+    vector<int> result = reverseLevelOrder(root);
+
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << " ";
+    }
+
+
+    return 0;
+}
