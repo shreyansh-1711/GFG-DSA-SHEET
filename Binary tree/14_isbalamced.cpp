3e0f2851c2214d5957ee2b87c8cef58a8036707c
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-27 00:19:34 +0530

    update

---
+++ Binary tree/14_isbalamced.cpp
@@ -0,0 +1,39 @@
+// class Solution{
+//         private:
+//     int height(struct Node* node){
+//         // code here 
+//         if(node == NULL){
+//             return 0;
+//         }
+        
+//         int left = height(node->left);
+//         int right = height(node->right);
+//         int ans = max(left, right) +1;
+//         return ans;
+//     }
+//     public:
+//     //Function to check whether a binary tree is balanced or not.
+//     bool isBalanced(Node *root)
+//     {
+//         //  Your Code here
+//         if(root == NULL){
+//             return true;
+//         }
+
+        
+//         bool left = isBalanced(root->left);
+//         bool right = isBalanced(root->right);
+//         bool diff = abs(height(root->left)-height(root->right)) <= 1;
+        
+//         if(left && right && diff){
+//             return 1;
+//         }else{
+//             return false;
+//         }
+        
+//     }
+// };
+
+
+
+// more optimized
