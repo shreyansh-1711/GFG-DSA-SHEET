3780fbe41cbd0c9dbfed7ef743c71d31e0b3b15a
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-03-30 22:10:45 +0530

    update

---
+++ Binary tree/10_right_view.cpp
@@ -0,0 +1,23 @@
+// void solve(Node* root, vector<int> &ans, int lvl){
+//     if(root==NULL){
+//         return ;
+//     }
+    
+//     if(lvl == ans.size()){
+//         ans.push_back(root->data);
+//     }
+//     solve(root->right, ans, lvl+1);
+//     solve(root->left, ans, lvl+1);
+
+    
+    
+// }
+
+// //Function to return a list containing elements of left view of the binary tree.
+// vector<int> leftView(Node *root)
+// {
+//    // Your code here
+//    vector<int> ans;
+//    solve(root, ans ,0);
+//    return ans;
+// }
