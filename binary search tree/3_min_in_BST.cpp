d261c267ea459d3ace5459cd8058742bbbc6b005
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-04-15 21:21:58 +0530

    BST started

---
+++ binary search tree/3_min_in_BST.cpp
@@ -0,0 +1,14 @@
+// int minValue(Node* root) {
+//     // Code here
+//         Node* curr=root;
+
+//     if(curr==NULL)
+
+//     return -1;
+
+//     while(curr->left)
+
+//     curr=curr->left;
+
+//     return curr->data;
+// }
