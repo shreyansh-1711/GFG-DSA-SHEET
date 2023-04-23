37ddf858a8598ab9b85c663208bfec9553181cc7
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-04-23 13:15:15 +0530

    Maximum sum of non-adjacent elements

---
+++ DP/Maximum sum of non-adjacent elements.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h> 
+
+int solve(vector<int> &nums, int n){
+    if(n<0){
+        return 0;
+    }
+    if(n==0){
+        return nums[0];
+    }
+    int incl = solve(nums, n-2) + nums[n];
+    int excl = solve(nums, n-1) + 0;
+    return max(incl, excl);
+}
+
+int solveMem(vector<int> &nums, int n, vector<int>&dp){
+    if(n<0){
+        return 0;
+    }
+    if(n==0){
+        return nums[0];
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    int incl = solve(nums, n-2) + nums[n];
+    int excl = solve(nums, n-1) + 0;
+    dp[n] = max(incl, excl);
+    return dp[n];
+}
+
+int solveTab(vector<int> &nums){
+    int n=nums.size();
+    vector<int> dp(n,0);
+    dp[0] = nums[0];
+    for(int i=1;i<n;i++){
+        int incl = dp[i-2]+nums[i];
+        int excl = dp[i-1];
+        dp[i] =  max(incl, excl);
+    }
+    return dp[n-1];
+
+}
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    // int n = nums.size();
+    // int ans = solve(nums, n-1);
+    // return ans;
+
+    // int n = nums.size();
+    // vector<int> dp(n,-1);
+    // return solveMem(nums, n-1, dp);
+    return solveTab(nums);
+
+
+}
