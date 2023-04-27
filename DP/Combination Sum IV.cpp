bc19b9093985bea9e289558f23a500805b67803c
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-04-27 16:42:04 +0530

    Combination Sum IV

---
+++ DP/Combination Sum IV.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h> 
+int solve(vector<int> &num, int tar){
+    if(tar==0){
+        return 1;
+    }
+    if(tar<0){
+        return 0;
+    }
+    int ans = 0;
+    for(int i=0; i<num.size(); i++)
+    {
+        ans += solve(num, tar-num[i]);
+    }
+    return ans;
+}
+int solveMem(vector<int> &num, int tar, vector<int>& dp){
+    if(tar==0){
+        return 1;
+    }
+    if(tar<0){
+        return 0;
+    }
+    if(dp[tar]!=-1){
+        return dp[tar];
+    }
+    int ans=0;
+    for(int i=0; i<num.size(); i++)
+    {
+       ans += solveMem(num, tar-num[i], dp);
+    }
+    dp[tar] = ans;
+    return dp[tar];
+}
+
+int solveTab(vector<int> &num, int tar ){
+   vector<int> dp(tar+1, 0);
+   dp[0] = 1;
+   for(int i=1;i<=tar; i++){
+       for(int j=0; j<num.size(); j++){
+           if(i-num[j]>=0)
+           dp[i] +=dp[i-num[j]];
+       }
+   }
+   return dp[tar];
+}
+
+
+
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+    // return solve(num, tar);
+    // vector<int>  dp(tar+1, -1);
+    // return solveMem(num, tar, dp);
+    return solveTab(num, tar);
+
+}
