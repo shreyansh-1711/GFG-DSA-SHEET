0e19225c4ac10555469ad4ae7685eb2bb2a5d2da
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-05-01 18:58:28 +0530

    dp with binary search

--- DP/Longest Increasing Subsequence.cpp
+++ DP/Longest Increasing Subsequence.cpp
@@ -131,6 +131,22 @@ class Solution
         }
         return dp[0][0];
     }
+        int solveoptimal(int n, int a[]){
+        if(n==0){
+            return 0;
+        }
+        vector<int> ans;
+        ans.push_back(a[0]);
+        for(int i=1;i<n;i++){
+            if(a[i]>ans.back()){
+                ans.push_back(a[i]);
+            }else{
+                int index = lower_bound(ans.begin(), ans.end(), a[i]) - ans.begin();
+                ans[index] = a[i];
+            }
+        }
+        return ans.size();
+    }
 
     int longestSubsequence(int n, int a[]){
 
