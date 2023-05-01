c2836df94d183e3baeeacad24d990c691d351ffb
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-05-01 11:34:16 +0530

    tabulation

--- DP/Largest square formed in a matrix.cpp
+++ DP/Largest square formed in a matrix.cpp
@@ -40,6 +40,30 @@ public:
         }
     }
     
+        int solveTab(vector<vector<int>>& mat, int &maxi){
+        int row = mat.size();
+        int col = mat[0].size();
+        vector<vector<int>> dp(row+1, vector<int> (col+1, 0));
+        
+        for(int i=row-1; i>=0; i--){
+            for(int j=col-1;j>=0; j--){
+                int right = dp[i][j+1];
+                int diag = dp[i+1][j+1];
+                int down = dp[i+1][j];
+                
+                if(mat[i][j]==1){
+                    dp[i][j] = 1+min(right, min(diag, down));
+                    maxi = max(maxi, dp[i][j]);
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return dp[0][0];
+        
+    }
+    
+    
     
     
     
