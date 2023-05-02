0a89dfccb426a1fcf7fbac7e326f8a4577502bee
Shreyansh Jain <86216930+shreyansh-1711@users.noreply.github.com>
2023-05-02 20:07:43 +0530

    top down gives TLE

---
+++ DP/Longest Arithmetic Progression.cpp
@@ -0,0 +1,33 @@
+class Solution{   
+public:
+    int solve(int index,int diff, int A[],  unordered_map<int, int> dp[]){
+        if(index<0){
+            return 0;
+        }
+        if(dp[index].count(diff)){
+            return dp[index][diff];
+        }
+        int ans=0;
+        for(int j=index-1; j>=0;j--){
+            if(A[index]-A[j]==diff){
+                ans = max(ans,1+ solve(j, diff, A, dp));
+            }
+        }
+        return dp[index][diff] = ans;
+    }
+    int lengthOfLongestAP(int A[], int n) {
+        // code here
+        if(n<=2){
+            return n;
+        }
+        unordered_map<int, int> dp[n+1];
+        
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                ans= max(ans, 2+solve(i, A[j]-A[i], A, dp));
+            }
+        }
+        return ans;
+    }
+};
